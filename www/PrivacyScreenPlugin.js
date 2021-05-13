18729925329be4e880f0095e48bb331b77dbaaae
Leonardo Fernandes <leonardo.monteiro.fernandes@gmail.com>
2021-05-14 01:17:26 +1000

    Fix plugin definition and exports

--- www/PrivacyScreenPlugin.js
+++ www/PrivacyScreenPlugin.js
@@ -2,10 +2,10 @@ var exec = require("cordova/exec");
 
 var PLUGIN_NAME = "PrivacyScreen";
 
-exports.enable = function (success, error) {
+module.exports.enable = function (success, error) {
 	exec(success, error, PLUGIN_NAME, "enable", []);
 };
 
-exports.disable: function (success, error) {
+module.exports.disable = function (success, error) {
 	exec(success, error, PLUGIN_NAME, "disable", []);
 };
